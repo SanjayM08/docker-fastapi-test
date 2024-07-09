93281e48dd394d778d936d8a440a8780b02cbb03
Sanjay M <114374318+SanjayM08@users.noreply.github.com>
2024-07-09 22:16:16 +0530

    Create script.sh

---
+++ script.sh
@@ -0,0 +1,97 @@
+clear
+apt update
+clear
+git clone https://github.com/SanjayM08/docker-fastapi-test.git
+cd docker-fastapi-test/
+clear
+s
+lcear
+ls
+clear
+ls
+apt install docker.io
+apt install python3
+pip install fastapi
+apt install python3-pip
+pip install fastapi
+virtualenv venv
+apt install python3-virtualenv
+virtualenv venv
+ls
+clear
+ls
+ls -la
+cd venv/
+ls
+cd ..
+rm venv/
+rm -rf venv/
+virtualenv venv
+ls
+pip install fastapi
+pip install fastapi uvicorn
+clear
+pip install fastapi
+cd venv/
+cd s
+la
+ls
+cd ..
+rm -rf venv/
+ls
+clear
+python3 -m venv myenv
+apt install python3.12-venv
+python3 -m venv myenv
+ls
+cd myenv/
+ls
+cd bin
+ls
+activate
+cd ..
+myenv\Scripts\activate
+clear
+ls
+cd myenv/
+ls
+cd bin/
+ls
+activate
+cd ../..
+ls
+source myenv/bin/activate
+pip install fastapi
+pip install fastapi uvicorn
+ls
+uvicorn main.py:app --reload --host 0.0.0.0
+clear
+ls
+apt install docker-compose
+docker-compose --build .
+docker-compose --build
+docker-compose . --build
+docker-compose build
+sudo systemctl start docker
+sudo systemctl status docker
+docker --version
+docker info
+sudo curl -L "https://github.com/docker/compose/releases/download/v2.18.1/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+python3 -m venv myenv
+source myenv/bin/activate
+pip install docker-compose
+pip install cython
+sudo apt-get update
+sudo apt-get install build-essential python3-dev
+pip install --no-cache-dir PyYAML
+pip install PyYAML==5.4.1
+deactivate
+rm -rf myenv
+python3 -m venv myenv
+source myenv/bin/activate
+pip install fastapi
+docker-compose build
+docker-compose up
+deactivate
+history
